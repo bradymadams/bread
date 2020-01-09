d09182fb4cc5a1e21b44c718e6f637cfbb7d9f2f
Brady Adams <brady.m.adams@gmail.com>
2020-01-08 21:33:03 -0700

    work on db models

---
+++ models.py
@@ -0,0 +1,44 @@
+
+from app import db
+
+class SpendCategory(db.Model):
+    __tablename__ = 'category'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=32))
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=32))
+    starting_balance = db.Column(db.Integer)
+    null_category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    null_category = db.relationship('SpendCategory')
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    amount = db.Column(db.Integer)
+    description = db.Column(db.String(length=128))
+    comments = db.Column(db.String(length=128))
+
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    account = db.relationship('Account', backref=db.backref('transactions'))
+    category = db.relationship('SpendCategory', backref=db.backref('transactions'))
+
+class Budget(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=32))
+
+class BudgetCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    months = db.Column(db.Integer)
+    amount = db.Column(db.Integer)
+
+    budget_id = db.Column(db.Integer, db.ForeignKey('budget.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    budget = db.relationship('Budget', backref=db.backref('categories'))
+    category = db.relationship('SpendCategory')
